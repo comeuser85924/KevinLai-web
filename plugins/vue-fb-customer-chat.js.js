dac3d5a2264fac52d53f61c54dfb20a7cee75a0b
kevinlai0924 <kevin@verywed.com>
2019-10-12 00:38:55 +0800

    clean

--- plugins/vue-fb-customer-chat.js.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import VueFbCustomerChat from 'vue-fb-customer-chat'
- 
-Vue.use(VueFbCustomerChat, {
-  page_id: 113720326704164, //  change 'null' to your Facebook Page ID,
-  theme_color: '#6699cc', // theme color in HEX
-  locale: 'en_US', // default 'en_US'
-})
